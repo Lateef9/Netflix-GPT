76fe9d6147e0ad37b94b34fca3ff8d7c331060e2
Shaik Lateef <shaiklateef7761@gmail.com>
2024-02-14 23:54:59 +0530

    GPT search & AddGPtMovie Slice

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,14 +1,78 @@
-import React from 'react'
+import React, { useRef } from "react";
+import openai from "../utills/openAi";
+import { API_OPTIONS } from "../utills/constants";
+import { useDispatch } from "react-redux";
+import { addGptMovieResult } from "../utills/gptSlice";
 
 const GptSearchBar = () => {
-  return (
-    <div className='pt-[8%] flex justify-center'>
-        <form  className=' w-1/2 bg-black grid grid-cols-12'  onSubmit={(e) => {e.preventDefault()}}>
-            <input type="text"  className="p-3 m-3 col-span-9" placeholder='what do you want to watch today?' />
-            <button className='py-3 px-3 m-3 bg-red-700 rounded-lg text-white col-span-3 '>Search</button>
-        </form>
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  const searchText = useRef(null);
 
-export default GptSearchBar
+  //for each movie i call tmdb searach movie API
+
+  const searchMovieTMDB = async (movie) => {
+    const data = fetch(
+      "https://api.themoviedb.org/3/search/movie?query=" +
+        movie +
+        "&include_adult=false&language=en-US&page=1",
+      API_OPTIONS
+    );
+
+    const json = data.json();
+    return json.results;
+  };
+
+  const handleGPTSearchClick = async () => {
+    console.log(searchText.current.value);
+    // Api call to get the movie results
+
+    const gptQuery =
+      "Act as a Movie Recommendation system and suggest some movies for the query : " +
+      searchText.current.value +
+      ". only give me names of 5 movies, comma seperated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya";
+
+    const gptResults = await openai.chat.completions.create({
+      messages: [{ role: "user", content: gptQuery }],
+      model: "gpt-3.5-turbo",
+    });
+
+    const gptMovies = gptResults.choices[0]?.message?.content.split(",");
+
+    const promiseArray = gptMovies.map((movie) => searchMovieTMDB(movie));
+    // [Promise, Promise, Promise, Promise, Promise]
+
+    const tmdbResults = await Promise.all(promiseArray);
+
+    console.log(tmdbResults);
+
+    dispatch(
+      addGptMovieResult({ movieNames: gptMovies, movieResults: tmdbResults })
+    );
+  };
+
+  return (
+    <div className="pt-[8%] flex justify-center">
+      <form
+        className=" w-1/2 bg-black grid grid-cols-12"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          ref={searchText}
+          type="text"
+          className="p-3 m-3 col-span-9"
+          placeholder="what do you want to watch today?"
+        />
+        <button
+          className="py-3 px-3 m-3 bg-red-700 rounded-lg text-white col-span-3 "
+          onClick={handleGPTSearchClick}
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default GptSearchBar;
--- src/utills/constants.js
+++ src/utills/constants.js
@@ -10,3 +10,5 @@ export const API_OPTIONS ={
   };
 
   export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500"
+
+  export const OPENAI_KEY = "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"
